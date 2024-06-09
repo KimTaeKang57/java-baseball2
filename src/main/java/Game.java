82448174a6bad930a603461089056c1a858c0ffe
unknown <ktk9757@kyonggi.ac.kr>
2024-06-09 21:31:49 +0900

    자동차 경주 도메인 생성

---
+++ src/main/java/Game.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class Game {
+    private List<Car> cars;
+
+    public Game(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+}
