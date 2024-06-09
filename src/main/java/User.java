f417b84f7cd27ebe643099d8dda48fd62079bbf2
unknown <ktk9757@kyonggi.ac.kr>
2024-06-09 21:42:31 +0900

    사용자 클래스에 게임 클래스 객체 삽입

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -19,6 +19,9 @@ public class User {
         System.out.println("시도할 회수는 몇회인가요?");
         int rounds = scanner.nextInt();
 
+        Game game = new Game(cars);
+        game.startGame(rounds);
+
         System.out.println("실행 결과");
 
         scanner.close();
